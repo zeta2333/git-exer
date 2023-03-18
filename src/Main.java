a456608883e04a67aba561a5f552bdaec2c393c3
Pycro <2023747015@qq.com>
2023-03-18 20:03:51 +0800

    main update2

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Main updated");
+        System.out.println("Main updated2");
     }
 }
