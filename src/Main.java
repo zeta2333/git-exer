873c62f5f05f523631fb58ba31bc7810aa84a922
Pycro <2023747015@qq.com>
2023-03-18 19:51:11 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author Pycro
+ * @version 1.0
+ * ${YEAR}-${MONTH}-${DAY} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
